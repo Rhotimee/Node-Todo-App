953d362b19bccaaf6b7b029b554e8197742d6b40
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2018-01-12 17:57:19 +0100

    mongodb add & delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,35 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+let obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) =>{
     if (err){
         return console.log('Unable to connect to MongoDB server')
     }
     console.log('Connected to MongoDB server')
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Finish Reading Outliers',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err){
+    //         return console.log('Unable to Insert todo', err)
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+
+    // db.collection('Users').insertOne({
+    //     name: 'Rotimi',
+    //     age: '23',
+    //     location: 'lagos',
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to Insert User');
+    //     }
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+    // })
 
     db.close();
 });
