d8d916fbb92fff3ec73011573b805aa2ed225eb4
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2018-01-13 22:06:04 +0100

    Added GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {user} = require('./models/user');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let app = express();
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {user} = require('./models/user');
+
+const app = express();
 
 app.use(bodyParser.json());
 
+// post todos
 app.post('/todos', (req, res) => {
         let todo = new Todo({
         text: req.body.text
@@ -23,6 +26,7 @@ app.post('/todos', (req, res) => {
     });
 }); 
 
+// Get all Todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
@@ -30,6 +34,23 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e); 
     });
 });
+
+// Get Individual Todo
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        // console.log('404')
+        res.status(404).send()
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            // console.log(JSON.stringify(todo, undefined, 2))
+            return res.status(404).send()
+        } res.send({todo});
+    }, (e) => {
+        res.status(400).send()
+    })
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000')
