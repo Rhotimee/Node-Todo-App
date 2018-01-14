be2059630fcd44053ef910ead346ae66d497f7e1
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2018-01-14 16:42:06 +0100

    Added test for PATCH /todo/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,10 +7,14 @@ const {Todo} = require('./../models/todo');
 
 const todos = [{
     _id: new ObjectID(),
-    text: 'First test todo'
+    text: 'First test todo',
+
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 1912
+
 }];
 
 beforeEach((done) => {
@@ -131,6 +135,46 @@ describe('POST /todos', () => {
             .end(done)
         })
         
+    })
+
+    describe('PATCH /todo/:id', () => {
+        it('should update the todo', (done) => {
+            let hexId = todos[0]._id.toHexString();
+            let text = 'This should be a new text'
+
+            request(app)
+                .patch(`/todos/${hexId}`)
+                .send({
+                    completed: true,
+                    text
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todo.text).toBe(text);
+                    expect(res.body.todo.completed).toBe(true);
+                    expect(typeof res.body.todo.completedAt).toBe('number');
+                })
+                .end(done)
+        });
+        it ('should clear completedAt when todo is completed', (done) => {
+            let hexId = todos[1]._id.toHexString();
+            let text = 'This should be Another new text !!'
+
+            request(app)
+                .patch(`/todos/${hexId}`)
+                .send({
+                    completed: false,
+                    text
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todo.text).toBe(text);
+                    expect(res.body.todo.completed).toBe(false);
+                    expect(res.body.todo.completedAt).toBeFalsy();
+                })
+                .end(done)
+
+        })
     })
     
 });
